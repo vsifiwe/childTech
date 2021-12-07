85cbf156151c133f75d401a4175245571c28b4b6
Manzi <asifiwemanzi@gmail.com>
2021-12-07 14:07:50 +0200

    Final

--- childTech/urls.py
+++ childTech/urls.py
@@ -47,8 +47,8 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     url(r'^swagger(?P<format>\.json|\.yaml)$',
         schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'', schema_view.with_ui('swagger',
+    path('', schema_view.with_ui('swagger',
         cache_timeout=0), name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc',
+    path('redoc/', schema_view.with_ui('redoc',
         cache_timeout=0), name='schema-redoc'),
 ]
