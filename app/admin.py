9693277477779f404cc2e8113de1dc8c1244ce2f
Manzi <asifiwemanzi@gmail.com>
2021-12-08 12:18:27 +0200

    register models in admin

--- app/admin.py
+++ app/admin.py
@@ -4,6 +4,7 @@ from .models import *
 admin.site.register(Program)
 admin.site.register(Course)
 admin.site.register(Lesson)
-
-
+admin.site.register(Enroll)
+admin.site.register(Appointment)
+admin.site.register(Event)
 # Register your models here.
